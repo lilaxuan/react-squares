2e76c5a7a04d3e19022e32d13a56e4e839e19773
Jiaxuan <li.10089@osu.edu>
2024-02-29 11:30:05 -0800

    useState for the box background color

--- Jiaxuan-Li-React-Counter/src/ChildBox.jsx
+++ Jiaxuan-Li-React-Counter/src/ChildBox.jsx
@@ -1,11 +1,22 @@
-import './ChildBoxStyle.css'
+import './ChildBoxStyle.css';
+import { useState } from 'react';
 
 
 export default function ChildBox() {
+    // use state management for the box color
+    const [backgroundColor, setBackgroundColor] = useState('white-background');
+
+    function changeBackgroundColor() {
+        if (backgroundColor === 'white-background') {
+            setBackgroundColor('black-background');
+        } else {
+            setBackgroundColor('white-background');
+        }
+    }
 
     return (
         <div>
-            <div class='box'> I'm a box</div>
+            <div className={`box ${backgroundColor}`} onClick={() => changeBackgroundColor()}></div>
         </div>
     )
 }
