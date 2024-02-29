16db84a294bd483d1bc62e3beefc092d8e297e5d
Jiaxuan <li.10089@osu.edu>
2024-02-29 11:09:26 -0800

    Create a Child Box React Component

---
+++ Jiaxuan-Li-React-Counter/src/ChildBox.jsx
@@ -0,0 +1,11 @@
+import './ChildBoxStyle.css'
+
+
+export default function ChildBox() {
+
+    return (
+        <div>
+            <div class='box'> I'm a box</div>
+        </div>
+    )
+}
--- Jiaxuan-Li-React-Counter/src/main.jsx
+++ Jiaxuan-Li-React-Counter/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Home from './Home.jsx'
+import ChildBox from './ChildBox.jsx'
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <Home />
+    <ChildBox />
   </React.StrictMode>,
 )
